31a77d73b2bbded7365e5c204193371cc17cb06f
Autumms <122081930+sp0zx@users.noreply.github.com>
2025-08-17 11:40:14 -0400

    Update index.lua

--- public/index.lua
+++ public/index.lua
@@ -8,6 +8,13 @@ local DexKeys = {
   "AD275A5E-A797-4C7B-B605-4FDE30657B8C"
 }
 
-if UserKey == UserClientID then
-  print("key and hwid matches")
+-- Check if the key is whitelisted
+if table.find(DexKeys, UserKey) then
+  if UserKey == UserClientID then
+    print("HWID matches key")
+  else
+    print("HWID does not match key")
+  end
+else
+  print("Key not found")
 end

92aad6b8779a8ee20b209c0e99de76a5db84b1bc
Autumms <122081930+sp0zx@users.noreply.github.com>
2025-08-17 11:25:24 -0400

    Update index.lua

--- public/index.lua
+++ public/index.lua
@@ -1,1 +1,11 @@
-print("HELLOOOO!!!")
+if not _G.Dex_Key then return end
+
+local DexKeys = {
+  "12345"
+}
+
+if table.find(DexKeys,tostring(_G.Dex_Key) then
+    print("Whitelisted")
+  else
+    print("Not whitelisted")
+  end

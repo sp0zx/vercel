af771be54128de9cdc4b15f1aaa98091a4a14dff
Autumms <122081930+sp0zx@users.noreply.github.com>
2025-08-17 11:33:57 -0400

    Update index.lua

--- public/index.lua
+++ public/index.lua
@@ -1,11 +1,13 @@
 if not _G.Dex_Key then return end
 
+local RAS = game:GetService("RbxAnalyticsService")
+local UserClientID = RAS:GetClientId()
+local UserKey = tostring(_G.Dex_Key)
+
 local DexKeys = {
-  "12345"
+  "AD275A5E-A797-4C7B-B605-4FDE30657B8C"
 }
 
-if table.find(DexKeys, tostring(_G.Dex_Key)) then
-  print("Whitelisted")
-else
-  print("Not whitelisted")
+if UserKey == UserClientID then
+  print("key and hwid matches")
 end
